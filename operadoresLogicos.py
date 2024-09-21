ac738b3d61a5affceb37174f9f6f68a4da988c98
Márcio Ferreira Lima <contactmarcioflima@gmail.com>
2024-09-20 22:16:58 -0300

    Update operadoresLogicos.py

--- operadoresLogicos.py
+++ operadoresLogicos.py
@@ -1,11 +1,64 @@
 # 1. Utilizando a função "print", verifique se dois números são iguais utilizando o operador de igualdade ("=="). 
 
-print(3 == 3)
+print('O valor 3 é igual a 3? ',3 == 3)
 
 # 2. Utilizando a função "print", verifique se dois números são diferentes utilizando o operador de diferença ("!=").
 
-print(3 != 7)
+print('O valor 7 é diferente de 3? ',3 != 7)
 
 # 3. Utilizando a função "print", verifique se um número é maior que outro utilizando o operador de maior que (">"). 
 
-print(7 > 3)
+print('O valor 7 é maior que 3? ', 7 > 3)
+
+# 4. Utilizando a função "print", verifique se um número é menor que outro utilizando o operador de
+# menor que ("<").
+
+print('O valor 7 é menor que 3? ', 7 < 3)
+
+# 5. Utilizando a função "print", verifique se um número é maior ou igual a outro utilizando o operador
+# de maior ou igual a (">=").
+
+print('O valor 3 é maior ou igual a 3? ', 3 >= 3)
+
+# 6. Utilizando a função "print", verifique se um número é menor ou igual a outro utilizando o operador
+# de menor ou igual a ("<=").
+
+print('O valor 4 é menor ou a igual 3?', 4 <= 3)
+
+# 7. Utilizando a função "print", verifique se duas condições são verdadeiras utilizando o operador
+# lógico AND ("and").
+
+print('O valor 2 e 3 são diferentes e são menores que 1? ', 2 != 3 and 3 < 1 and 2 < 1)
+
+# 8. Utilizando a função "print", verifique se pelo menos uma das condições é verdadeira utilizando o
+# operador lógico OR ("or").
+
+print('O valor 2 e 3 são diferentes ou são menores que 1? ', 2 != 3 or 3 < 1 or 2 < 1)
+
+# 9. Utilizando a função "print", negue uma variável booleana utilizando o operador lógico NOT ("not").
+
+A = True
+print(not A)
+
+# 10. Utilizando a função "print", verifique se um número está dentro de um determinado intervalo,
+# utilizando os operadores lógicos AND e os operadores de comparação (">=" e "<=").
+
+print('O numero 5 está entre 0 e 10? ', 0 <= 5 <= 10)
+
+# 11. Utilizando a função "print", verifique se um número está fora de um determinado intervalo,
+# utilizando os operadores lógicos OR e os operadores de comparação ("<" e ">").
+
+print('O numero 5 está no intervalo maior que 10? ', 5 >= 10)
+
+# 12. Utilizando a função "print", verifique se uma string está vazia. Utilize o operador de igualdade ("==")
+# e a função "len" para verificar se o tamanho da string é igual a zero.
+
+vazio = 'zero'
+print("A variavel 'vazio' está vazia? ", len(vazio) == 0)
+
+# 13. Utilizando a função "print". Verifique se uma variável é do tipo booleano. Utilize a função "type"
+# para obter o tipo da variável e o operador de igualdade ("==") para comparar se o tipo de dado é
+# "bool".
+
+booleana = True
+print("A variavel booleana é do tipo 'bool'? ", type(booleana) == bool)
